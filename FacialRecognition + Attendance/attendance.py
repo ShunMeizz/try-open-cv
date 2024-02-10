1fe5a672e2da4758e08f0db9a41491f109e0716f
ShunMeizz <sebialshanleyflores@gmail.com>
2024-02-11 03:31:08 +0800

    update

---
+++ FacialRecognition + Attendance/attendance.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+import face_recognition as fr
+import os
+
+path = 'FacesAttendance'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+
+for cl in myList:  # load files into our custom array images[] and classNames[]
+    currImg = cv2.imread(f'{path}/{cl}')
+    images.append(currImg)
+    classNames.append(os.path.splitext(cl)[0])  # [0] it will grab the first element, not the extension (.jpg) which is last
+print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # convert color format
+        encode = fr.face_encodings(img)[0] # encode
+        encodeList.append(encode)
+    return encodeList
+
+encodeListKnown = findEncodings(images)
+print("Encoding complete")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    #in the webcam image we might actually find multiple faces so for that we need to find the lcation of our faces and send
+    #this locations to our face_encodings function
+
+    facesCurrFrame = fr.face_locations(imgS)
+    encodesCurrFrame = fr.face_encodings(imgS,facesCurrFrame)
+
+    #one by one it will grab one faceLoc from facesCurrFrame list and then it will grab the encodeFace in the encodesCurrFrame
+    for encodeFace, faceLoc in zip (encodesCurrFrame, facesCurrFrame):
+        matches = fr.compare_faces(encodeListKnown, encodeFace)
+        faceDis = fr.face_distance(encodeListKnown, encodeFace)
+        matchIndex = np.argmin(faceDis)
--- FacialRecognition + Attendance/main.py
+++ FacialRecognition + Attendance/main.py
@@ -1,1 +1,44 @@
+import cv2 #BGR as default color format
+import numpy as np
+import face_recognition as fr # can only understand RGB color format image
+
+imgElon = fr.load_image_file("Images/elon.jpg")
+imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
+imgElon = cv2.resize(imgElon,(510,800))
+imgElonTest = fr.load_image_file("Images/elon_musk.jpg")
+imgElonTest = cv2.cvtColor(imgElonTest, cv2.COLOR_BGR2RGB)
+imgHawkingTest = fr.load_image_file("Images/stephen_hawking.jpg")
+imgHawkingTest = cv2.cvtColor(imgHawkingTest, cv2.COLOR_BGR2RGB)
+
+#[0] kay we are only sending a single image, we will just get the first element of this
+faceLoc = fr.face_locations(imgElon)[0]
+encodeElon = fr.face_encodings(imgElon)[0]
+cv2.rectangle(imgElon,(faceLoc[3], faceLoc[0]),(faceLoc[1], faceLoc[2]),(255,0,255),2)
+
+faceLocTest = fr.face_locations(imgElonTest)[0]
+encodeElonTest = fr.face_encodings(imgElonTest)[0]
+cv2.rectangle(imgElonTest,(faceLocTest[3], faceLocTest[0]),(faceLocTest[1], faceLocTest[2]),(255,0,255),2)
+
+faceLocTest2 = fr.face_locations(imgHawkingTest)[0]
+encodeHawkingTest = fr.face_encodings(imgHawkingTest)[0]
+cv2.rectangle(imgHawkingTest,(faceLocTest2[3], faceLocTest2[0]),(faceLocTest2[1], faceLocTest2[2]),(255,0,255),2)
+
+compare_res1 = fr.compare_faces([encodeElon], encodeElonTest)
+compare_res2 = fr.compare_faces([encodeElon], encodeHawkingTest)
+faceDis = fr.face_distance([encodeElon], encodeElonTest)
+faceDis2 = fr.face_distance([encodeElon], encodeHawkingTest)
+print(compare_res1, faceDis)
+print(compare_res2, faceDis2)
+
+cv2.putText(imgElonTest,f'{compare_res1}, {round(faceDis[0],2)}', (50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+cv2.putText(imgHawkingTest,f'{compare_res2}, {round(faceDis2[0],2)}', (50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+
+cv2.imshow("Elon Musk", imgElon)
+cv2.waitKey(0)
+cv2.imshow("StephenHawking Test Subject", imgHawkingTest)
+cv2.waitKey(0)
+cv2.imshow("ElonMusk Test Subject", imgElonTest)
+cv2.waitKey(0)
+
+
 
